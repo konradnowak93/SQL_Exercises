a4e1b52aa8e34cc82ba0650bdd4878ced32e9094
zabson93 <konrad.novakk@gmail.com>
2019-04-18 10:34:10 +0200

    Readability improved.

--- PD_2.sql
+++ PD_2.sql
@@ -1,25 +1,37 @@
 Napisz w języku SQL zapytania wyświetlające następujące informacje z bazy danych czek:
 
 a)identyfikator zamówienia i liczbę pełnych dni, jakie upłynęły od daty tego zamówienia,
-select id_zam, datediff(curdate(), data_zam) as 'Dni od zamówienia' 
+select id_zam, 
+	datediff(curdate(), 
+	data_zam) as 'Dni od zamówienia' 
 from czek.zamowienia;
 
 b)dla każdego zamówienia z października, które opłacono przelewem, wyświetlić identyfikatory zamówienia, dostawcy i klienta,
-select id_zam, id_klienta, id_dostawcy, forma_zaplaty 
+select id_zam, 
+	id_klienta, 
+	id_dostawcy, 
+	forma_zaplaty 
 from czek.zamowienia 
-where extract(month from data_zam) = 10 and forma_zaplaty = 'p';
+where extract(month from data_zam) = 10 
+	and forma_zaplaty = 'p';
 
 c)imiona i nazwiska klientów, którzy mieszkają w mieście o kodzie zaczynającym się na 70 lub zaczynającym się i kończącym na 1,
-select imie, nazwisko, miasto, kod 
+select imie, 
+	nazwisko, 
+	miasto, 
+	kod 
 from czek.klienci 
-where kod like '70%' or kod like '1____1';
+where kod like '70%' 
+	or kod like '1____1';
 
 d)miasta, z których pochodzą klienci - bez powtórzeń,
 select distinct miasto 
 from czek.klienci;
 
 e)maksymalną, minimalną oraz średnią cenę pudełek z czekoladkami
-select max(cena_pudelka) as 'Maksymalna cena pudełka', min(cena_pudelka) as 'Minimalna cena pudełka', avg(cena_pudelka) as 'Średnia cena pudełka' 
+select max(cena_pudelka) as 'Maksymalna cena pudełka', 
+	min(cena_pudelka) as 'Minimalna cena pudełka', 
+	avg(cena_pudelka) as 'Średnia cena pudełka' 
 from czek.pudelka;
 
 f)średni koszt czekoladek, w których opisie występuje słowo 'migdał',
@@ -28,11 +40,13 @@ from czek.czekoladki
 where opis like '%migdał%';
 
 g)nazwy regionów i liczbę klientów z tych regionów uporządkowane alfabetycznie według regionów,
-select region, count(*) as 'Liczba klientów' 
+select region, 
+	count(*) as 'Liczba klientów' 
 from czek.klienci 
 group by region;
 
 h)identyfikatory klientów i liczbę złożonych przez nich zamówień.
-select id_klienta, count(*) as 'Liczba zamówień' 
+select id_klienta, 
+	count(*) as 'Liczba zamówień' 
 from czek.zamowienia 
 group by id_klienta;
--- PD_4.sql
+++ PD_4.sql
@@ -1,55 +1,81 @@
 Napisz w języku SQL zapytania wyświetlające następujące informacje z bazy danych czek:
 
 	a) nazwę pudełka oraz liczbę zamówień, na których występuje,
-	select p.nazwa, count(sz.id_zam) as 'Liczba zamówień' 
-	from czek.pudelka p join czek.szczegolowe_zam sz on p.id_pudelka = sz.id_pudelka 
+	select p.nazwa, 
+		count(sz.id_zam) as 'Liczba zamówień' 
+	from czek.pudelka p 
+		join czek.szczegolowe_zam sz 
+			on p.id_pudelka = sz.id_pudelka 
 	group by p.nazwa;
 	
 	b) imiona i nazwiska dostawców wraz z liczbą dostarczonych zamówień, dla tych dostawców, którzy obsłużyli przynajmniej 15 zamówień,
-	select d.imie, d.nazwisko, count(z.id_zam) as 'Liczba obsłużonych zamówień' 
-	from czek.dostawcy d join czek.zamowienia z on d.id_dostawcy = z.id_dostawcy 
+	select d.imie, 
+		d.nazwisko, 
+		count(z.id_zam) as 'Liczba obsłużonych zamówień' 
+	from czek.dostawcy d 
+		join czek.zamowienia z 
+			on d.id_dostawcy = z.id_dostawcy 
 	group by z.id_dostawcy 
 	having count(id_zam) >= 15; 
 	
 	c) imiona i nazwiska klientów wraz z liczbą złożonych zamówień. Wypisz także klientów, którzy nie złożyli żadnego zamówienia.
-	select k.imie, k.nazwisko, count(z.id_zam) as 'Liczba złożonych zamówień'
-	from czek.klienci k left join czek.zamowienia z on k.id_klienta = z.id_klienta
+	select k.imie, 
+		k.nazwisko, 
+		count(z.id_zam) as 'Liczba złożonych zamówień'
+	from czek.klienci k left 
+		join czek.zamowienia z 
+			on k.id_klienta = z.id_klienta
 	group by k.id_klienta;
 	
 	d) dla pudełka o nazwie 'Kolekcja alpejska' wypisz nazwy czekoladek, które znajdują się w tym pudełku,
-	select p.nazwa as 'Nazwa pudełka', cz.nazwa as 'Nazwa czekoladek znajdujących się w pudełku'
+	select p.nazwa as 'Nazwa pudełka', 
+		cz.nazwa as 'Nazwa czekoladek znajdujących się w pudełku'
 	from czek.pudelka p 
-	left join czek.o_pudelkach op on p.id_pudelka = op.id_pudelka 
-	left join czek.czekoladki cz on op.id_czek = cz.id_czek
+		left join czek.o_pudelkach op 
+			on p.id_pudelka = op.id_pudelka 
+		left join czek.czekoladki cz 
+			on op.id_czek = cz.id_czek
 	where p.nazwa = 'Kolekcja alpejska';
 	
 	e) nazwy i stan pudełek, których jest najmniej w magazynie,
-	select nazwa, sztuk_w_magazynie
+	select nazwa, 
+		sztuk_w_magazynie
 	from czek.pudelka
 	where sztuk_w_magazynie = (select min(sztuk_w_magazynie) from czek.pudelka);
 	
 	
 	f) identyfikatory, imiona i nazwiska klientów, którzy nie złożyli żadnego zamówienia,
-	select k.id_klienta, k.imie, k.nazwisko, count(z.id_zam) as 'Liczba zamówien'
+	select k.id_klienta, 
+		k.imie, 
+		k.nazwisko, 
+		count(z.id_zam) as 'Liczba zamówien'
 	from czek.klienci k
-	left join czek.zamowienia z on k.id_klienta = z.id_klienta
+		left join czek.zamowienia z 
+			on k.id_klienta = z.id_klienta
 	group by k.id_klienta
 	having count(z.id_zam) = 0;
 	
 	
 	g) identyfikatory zamówień na najtańsze pudełko,
-	select z.id_zam, p.cena_pudelka 
-	from czek.zamowienia z join czek.szczegolowe_zam sz on z.id_zam = sz.id_zam 
-	join czek.pudelka p on czek.sz.id_pudelka = p.id_pudelka 
+	select z.id_zam, 
+		p.cena_pudelka 
+	from czek.zamowienia z 
+		join czek.szczegolowe_zam sz 
+			on z.id_zam = sz.id_zam 
+		join czek.pudelka p 
+			on czek.sz.id_pudelka = p.id_pudelka 
 	where p.cena_pudelka = (select min(p.cena_pudelka) from czek.pudelka);
 	
 	h) nazwy i koszt czekoladek oraz rodzaj czekolady, dla najdroższych czekoladek z każdego rodzaju czekolady,
-	select nazwa as 'Nazwa czekoladek', koszt as 'Koszt najdroższych czekoladek danego rodzaju', rodzaj_czekolady as 'Rodzaj czekolady'
+	select nazwa as 'Nazwa czekoladek', 
+		koszt as 'Koszt najdroższych czekoladek danego rodzaju', 
+		rodzaj_czekolady as 'Rodzaj czekolady'
 	from czek.czekoladki
 	where (rodzaj_czekolady, koszt) in (select rodzaj_czekolady, max(koszt) from czek.czekoladki group by rodzaj_czekolady)
 	
 	i) maksymalną liczbę sztuk (sumę) zamówionych pudełek z poszczególnych zamówień.
-	select id_zam, sum(ile_sztuk) 
+	select id_zam, 
+		sum(ile_sztuk) 
 	from szczegolowe_zam 
 	group by id_zam;
 	
